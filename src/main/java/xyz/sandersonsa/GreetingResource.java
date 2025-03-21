ce0d50190e1a7600b1e154c32ce331d5f1b2e8e1
C1343636 Sanderson Costa De Sa <c1343636@interno.bb.com.br>
2025-03-21 10:30:29 -0300

    get hostname

--- src/main/java/xyz/sandersonsa/GreetingResource.java
+++ src/main/java/xyz/sandersonsa/GreetingResource.java
@@ -1,5 +1,7 @@
 package xyz.sandersonsa;
 
+import java.util.Optional;
+
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
@@ -11,6 +13,10 @@ public class GreetingResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
+        Optional<String> podName = Optional.ofNullable(System.getenv("HOSTNAME"));
+        if (podName.isPresent()) {
+            System.out.println("Pod Hostname: " + podName.get());    
+        }        
         return "Hello from Quarkus REST";
     }
 }
